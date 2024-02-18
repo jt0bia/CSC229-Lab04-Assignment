10e8ff57bfd01dde41185d2e5ffde0df04363bd8
Jared Tobias <jaredtobias12@gmail.com>
2024-02-18 13:44:02 -0500

    Task02 completed

--- src/Task02.java
+++ src/Task02.java
@@ -1,2 +1,31 @@
 public class Task02 {
+
+    public static void main(String[] args) {
+        System.out.println(gcd(10, 15));
+    }
+
+    public static int gcd(int x, int y) {
+        if (x % y == 0) //base case
+            return y;
+        else
+            return gcd(y, x % y);
+    }
+
+    // Todo 1: What is the output of this code?
+    // For example, if x is 10, and y is 15, the output is:
+    // 5
+
+    // Todo 2: Modify it to find the gcd using subtraction instead of %
+    // To use subtraction the Euclidean algorithm can be applied
+    public static int gcdSubtraction(int x, int y) {
+        if (y == 0)
+            return y;
+        else
+            return gcdSubtraction(y, x - y);
+    }
+
+
+
+
+
 }
