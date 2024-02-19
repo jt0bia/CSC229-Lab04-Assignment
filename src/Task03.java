5c2b8ded384f82eb8c00e85c7cf5e7713b2bf31d
Jared Tobias <jaredtobias12@gmail.com>
2024-02-19 14:31:32 -0500

    Task03 completed

--- src/Task03.java
+++ src/Task03.java
@@ -1,2 +1,17 @@
 public class Task03 {
+    int fib(int n) {
+        if (n <= 0) // base case
+            return 0;
+        else if (n == 1) // base case
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+
+    // 1: What is the time complexity of this algorithm and why?
+    // The time complexity of this algorithm is exponential,
+    // or a Big O notation of O(2^n), because each time the
+    // function executes it will recursively call itself twice,
+    // which leads to exponential growth of calls and computations.
+
 }
